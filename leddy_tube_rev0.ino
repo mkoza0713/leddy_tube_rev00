96797f3ea56afdce2144b69a958b3d31102d6c5d
Mateusz Koza <mkoza0713@gmail.com>
2023-01-30 19:48:36 +0100

    Update hour settings
    Update problems with turning on light

--- leddy_tube_rev0.ino
+++ leddy_tube_rev0.ino
@@ -10,18 +10,18 @@ byte lockModeStage = 0;  //żeby nie zmieniać bez potrzeby stanu cały czas
 #define relay1 8
 
 
-void setup () {
+void setup() {
   Serial.begin(9600);
 
-  if (! rtc.begin()) {
+  if (!rtc.begin()) {
     Serial.println("Couldn't find RTC");
     Serial.flush();
     while (1) delay(10);
   }
 
-  if (! rtc.isrunning()) {
+  if (!rtc.isrunning()) {
     Serial.println("RTC is NOT running, let's set the time!");
-    lockRtc=0; //RTC zablokowane
+    lockRtc = 0;  //RTC zablokowane
     rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
   }
 
@@ -30,97 +30,102 @@ void setup () {
   //rtc.adjust(DateTime(2023, 1, 28, 20, 16, 0));
 
   pinMode(relay1, OUTPUT);
-
 }
 
-void loop () {
-    DateTime now = rtc.now();
+void loop() {
+  DateTime now = rtc.now();
 
-    if(lockRtc==1){
-      hourStage();
-      if(prevLightModeHour!=lightModeHour){
-        lockModeStage=0;
-      }
-      if(lockModeStage==0){
-        lockModeStage=1;
-        prevLightModeHour=lightModeHour;
-        lightMode(lightModeHour);
-      }
+  if (lockRtc == 1) {
+    hourStage();
+    if (prevLightModeHour != lightModeHour) {
+      lockModeStage = 0;
     }
+    if (lockModeStage == 0) {
+      lockModeStage = 1;
+      prevLightModeHour = lightModeHour;
+      lightMode(lightModeHour);
+    }
+  }
 
-    nowHour = now.hour();
-    nowMinute = now.minute();
-    // Serial.print(now.hour());
-    // Serial.print(':');
-    // Serial.print(now.minute());
-    // Serial.print(':');
-    // Serial.print(now.second());
-    // Serial.print("   prev Tryb pracy:");
-    // Serial.print(lightModeHour);
-    // Serial.print("   Tryb pracy:");
-    // Serial.println(lightModeHour);
-
+  nowHour = now.hour();
+  nowMinute = now.minute();
+  Serial.print(now.hour());
+  Serial.print(':');
+  Serial.print(now.minute());
+  Serial.print(':');
+  Serial.print(now.second());
+  Serial.print("   RTC:");
+  Serial.print(lockRtc);
+  Serial.print("   Tryb pracy:");
+  Serial.print(lightModeHour);
+  Serial.print("   lockModeStage:");
+  Serial.println(lockModeStage);
 }
-void hourStage(){
-  byte morningStartH=7;
-  byte morningStartM=0;
-  byte morningStopH=8;
-  byte morningStopM=0;
+void hourStage() {
+  byte morningStartH = 7;
+  byte morningStartM = 0;
+  byte morningStopH = 8;
+  byte morningStopM = 0;
 
-  byte afternoonStartH=8;
-  byte afternoonStartM=0;
-  byte afternoonStoptH=15;
-  byte afternoonStoptM=0;
+  byte afternoonStartH = 8;
+  byte afternoonStartM = 0;
+  byte afternoonStoptH = 15;
+  byte afternoonStoptM = 0;
 
-  byte eveningStartH =15 ;
+  byte eveningStartH = 15;
   byte eveningStartM = 0;
   byte eveningStopH = 17;
   byte eveningStopM = 0;
 
-  if(nowHour>=morningStartH && nowMinute>=morningStartM && nowHour<=morningStopH && nowMinute<morningStopM){
-    lightModeHour=1;
-  }
-  else if(nowHour>=afternoonStartH && nowMinute>=afternoonStartM && nowHour<=afternoonStoptH && nowMinute<afternoonStoptM){
-    lightModeHour=2;
-  }
-  else if(nowHour>=eveningStartH && nowMinute>=eveningStartM && nowHour<=eveningStopH && nowMinute<eveningStopM){
-    lightModeHour=3;
-  }
-  else lightModeHour=0;
+  byte nightStartH = 17;
+  byte nightStartM = 0;
+  byte nightStopH = 19;
+  byte nightStopM = 50;
+
+
+  if (nowHour >= morningStartH && nowMinute >= morningStartM && nowHour <= morningStopH && nowMinute < morningStopM) {
+    lightModeHour = 1;
+  } else if (nowHour >= afternoonStartH && nowMinute >= afternoonStartM && nowHour <= afternoonStoptH && nowMinute < afternoonStoptM) {
+    lightModeHour = 2;
+  } else if (nowHour >= eveningStartH && nowMinute >= eveningStartM && nowHour <= eveningStopH && nowMinute < eveningStopM) {
+    lightModeHour = 3;
+  } else if (nowHour >= nightStartH && nowMinute >= nightStartM && nowHour <= nightStopH && nowMinute < nightStopM) {
+    lightModeHour = 4;
+  } else lightModeHour = 0;
 }
-void lightMode(byte mode){
+void lightMode(byte mode) {
   digitalWrite(relay1, LOW);
   delay(20000);  //czekam zeby swietlowka zaczela od domyslnego
-  switch(mode){
+  switch (mode) {
     case 1:  //swiecenie poranne
       digitalWrite(relay1, HIGH);
       delay(1000);
       digitalWrite(relay1, LOW);
       delay(1000);
       digitalWrite(relay1, HIGH);
-    break;
-    case 2://swiecenie dzienne
+      break;
+    case 2:  //swiecenie dzienne
       digitalWrite(relay1, HIGH);
       delay(1000);
-    break;
-    case 3://swiecenie wieczorne
-      digitalWrite(relay1, HIGH);
-      delay(1000);
-      digitalWrite(relay1, LOW);
-      delay(1000);
-      digitalWrite(relay1, HIGH);
-    break;
-    case 4://swiecenie nocne
+      break;
+    case 3:  //swiecenie wieczorne
       digitalWrite(relay1, HIGH);
       delay(1000);
       digitalWrite(relay1, LOW);
       delay(1000);
+      digitalWrite(relay1, HIGH);
+      break;
+    case 4:  //swiecenie nocne
       digitalWrite(relay1, HIGH);
       delay(1000);
       digitalWrite(relay1, LOW);
       delay(1000);
       digitalWrite(relay1, HIGH);
       delay(1000);
-    break;
+      digitalWrite(relay1, LOW);
+      delay(1000);
+      digitalWrite(relay1, HIGH);
+      delay(1000);
+      break;
   }
 }
